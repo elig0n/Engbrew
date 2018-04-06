ff5a3c607be0e486e8cb75547f4abaddbd361b14
elig0n <eliran.gonen@gmail.com>
2018-04-07 01:32:05 +0300

    added laycon.py

---
+++ laycon.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# laycon.py - convert english characters to hebrew layout
+
+import sys
+
+lay = {
+        'q': '/', 'w':'\'', 'e': 'ק', 'r': 'ר', 't': 'א', 'y': 'ט', 'u': 'ו', 'i': 'ן', 'o': 'ם', 'p': 'פ',
+        'a': 'ש', 's': 'ד', 'd': 'ג', 'f': 'כ', 'g': 'ע', 'h': 'י', 'j': 'ח', 'k': 'ל', 'l': 'ף', '\'': ',', 
+        'z': 'ז', 'x': 'ס', 'c':'ב', 'v':'ה', 'b':'נ', 'n':'מ', 'm':'צ', ',':'ת', '.':'ץ', '/':'.'
+}
+
+try:
+    str = sys.argv[1]
+except IndexError:
+    print("No string was supplied.")
+
+for c in str:
+    if c in lay: 
+        print(lay[c], end='')
+    else:
+        print (c, end='')
+
+print()
